3c34eab6a369c07fc2134b22f28bd0b9d2b514e5
roshni1602 <verma1602rv@gmail.com>
2022-01-20 17:01:17 +0530

    Math Engine Capabilites

---
+++ Math.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace MathLib
+{
+    public class MathEngine
+    {
+        public static int Addition(int n1,int n2)
+        {
+            return n1+n2;
+        }
+
+        public static int Addition(int n1,int n2)
+        {
+            return n1-n2;
+        }
+
+    }
+}
+
+
+
+
+
