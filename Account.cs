473952063f55dccea8e249bd556db50fd15b556e
SwapnilPawar965 <98086836+SwapnilPawar965@users.noreply.github.com>
2022-01-21 15:05:57 +0530

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    class Account
+    {
+        private readonly int AccountID;
+        private String AccountHolder;
+        private double bal;
+
+        public Account()
+        {
+            AccountID = 0;
+            AccountHolder = null;
+            bal = 0;
+        }
+
+        public Account(int AccountID, String AccountHolder, double bal)
+        {
+            this.AccountID = AccountID;
+            this.AccountHolder = AccountHolder;
+            this.bal = bal;
+        }
+
+        public void withdraw(double amount)
+        {
+            bal = bal - amount;
+        }
+
+        public void deposit(double amount)
+        {
+            bal = bal + amount;
+        }
+
+        public double Bal
+        {
+            get 
+            {
+                return this.bal;
+            }
+            set
+            {
+                this.Bal = bal;
+            }
+        }
+
+        public String getAccountHolder()
+        {
+            return AccountHolder;
+        }
+      
+        public int getAccountID()
+        {
+            return AccountID;
+        }
+
+        public override string ToString()
+        {
+            return "New account balance = " + Bal;
+        }
+    }
+}
